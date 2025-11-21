94aa3eacbb3cf7e1932f07d9dab5ce2d55d8dfff
karishma9142 <karishmacair47050@gmail.com>
2025-11-21 23:25:19 +0530

    adding useEffect

--- react/react/src/App.jsx
+++ react/react/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react"
+import { useState , useEffect} from "react"
 
-
+// condition rendering
 function App() {
+  const [CounterVisible,setCounterVisible]=useState(true);
   return <div>
    <h3>hii there</h3>
-    <Counter></Counter>
+    {CounterVisible?<Counter></Counter>:null}
+    <button onClick={() => setCounterVisible(false)}>hide counter</button>
+    <button onClick={()=>setCounterVisible(true)}>Show counter</button>
   </div>
 }
 function Counter(){
   const [count,setCount] = useState(0);
 
+   // mounting
+  useEffect(function(){
+    setInterval(function(){
+      setCount(count => count+1)
+    },1000);
+
+    // unmounting
+    return function(){
+      clearInterval(count);
+    }
+  },[])
   function incCnt(){
     setCount(count+1);
   }
