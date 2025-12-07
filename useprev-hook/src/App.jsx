0821095955c835201fe2bc6e7a8a7c45bbeedcb5
karishma9142 <karishmacair47050@gmail.com>
2025-12-07 16:52:23 +0530

    add comments

--- useprev-hook/src/App.jsx
+++ useprev-hook/src/App.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useRef, useState } from 'react'
 
+// In React, the component renders first, and useEffect runs afterward.
+// That means usePrev first returns ref.current (the previous value, e.g., 2).
+// Then the useEffect hook runs and updates ref.current to the new value (e.g., 3).
+// For the initial count = 0, it returns undefined because there is no previous value yet.
+
 function usePrev(state) {
   const ref = useRef();
   useEffect (() =>{
     ref.current = state
   },[state]);
-  return ref.current;
+  return ref.current; 
 }
 
 function App() {
